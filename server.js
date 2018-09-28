1dedf934a1824da62e3fe69b3993981c9b18470d
jayarellano6 <jaarellano@csumb.edu>
2018-09-27 19:08:54 -0700

    k

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './ng6'));
+app.use(express.static(__dirname + '/dist/ng6'));
 
-app.get('*', function(req,res) {
+app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'./ng6/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ng6/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
